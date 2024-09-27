eb5cc6dc9049771402a6919811e15735abe8764f
fieffriends <48393262+fieffriends@users.noreply.github.com>
2024-09-27 21:26:05 +0000

    Deploying to gh-pages from @ Llamapedia/llamapedia.github.io@e806bd6fd4809ca37f8970311d73ae5a599ae2b3 🚀

---
+++ _nuxt/Ce2W345x.js
@@ -0,0 +1,62 @@
+import{n as A,r as u,u as E,q as L,h as j,c as l,s as o,a as i,t as g,v,F as y,A as b,o as d,p as B,e as V,B as m,_ as q}from"./VTd8GIPQ.js";import{p as O}from"./D4h8WKgh.js";const C=p=>(B("data-v-ed1964bd"),p=p(),V(),p),R={key:0,class:"manga-details"},D={class:"manga-banner"},G=["src"],x={class:"manga-title"},U={class:"manga-info"},z={class:"manga-cover"},J=["src"],H={class:"manga-text"},F=["innerHTML"],W={class:"manga-volumes"};const Y={key:2,class:"manga-completion manga-completion-wip"},K=C(()=>i("p",null,"Work in progress...",-1)),Q={key:3,class:"streaming-links"},X=C(()=>i("h2",{class:"streaming-links-headline"},"Read this manga:",-1)),Z={class:"streaming-service-title"},ee=["href"],se=A({__name:"[id]",setup(p){const s=u(),h=u(),f=u(),e=u(),k=E();L(()=>{const a=k.params.id.toString();$(parseInt(a)),w(parseInt(a))}),j(s,async(a,r)=>{a!==r&&(await I(),console.log(e.value))});function ae(){switch(e.value.status){case"COMPLETED":e.value.progress=s.value.episodes;break;case"CURRENT":e.value.progress=0;break;case"PLANNING":e.value.progress=0;break;case"REPEATING":e.value.progress=0;break}}async function T(){const a=m.get("access_token"),r=`
+            query {
+                Viewer {
+                    id
+                    name
+                }
+            }
+        `,n={},c=await fetch("https://graphql.anilist.co",{method:"POST",headers:{"Content-Type":"application/json",Accept:"application/json",Authorization:`Bearer ${a}`},body:JSON.stringify({query:r,variables:n})});if(c.status===200){const t=await c.json();console.log(t),f.value=t.data.Viewer}}async function $(a){const t=await(await fetch("https://graphql.anilist.co",{method:"POST",headers:{"Content-Type":"application/json",Accept:"application/json"},body:JSON.stringify({query:`
+        query ($id: Int, $type: MediaType) {
+            Media (idMal: $id, type: $type) {
+                id
+                idMal
+                title {
+                    romaji
+                    english
+                    native
+                }
+                coverImage {
+                    extraLarge
+                    large
+                    medium
+                    color
+                }
+                description
+                volumes
+                bannerImage
+            }
+        }
+    `,variables:{id:a,type:"MANGA"}})})).json();console.log(t),s.value=t.data.Media}async function I(){await T();const a=m.get("access_token"),r=`
+    query ($userId: Int, $type: MediaType) {
+            MediaListCollection(userId: $userId, type: $type) {
+                lists {
+                    name
+                    entries {
+                        status
+                        score
+                        progress
+                        mediaId
+                        media {
+                            idMal
+                        }
+                    }
+                }
+            }
+        }
+    `,n={userId:f.value.id,type:"MANGA"},t=await(await fetch("https://graphql.anilist.co",{method:"POST",headers:{"Content-Type":"application/json",Accept:"application/json",Authorization:`Bearer ${a}`},body:JSON.stringify({query:r,variables:n})})).json();for(const _ in t.data.MediaListCollection.lists){const M=t.data.MediaListCollection.lists[_].entries.find(P=>P.media.idMal===s.value.idMal);if(M){e.value=M;return}}}async function te(){const a=m.get("access_token"),r=`
+    mutation ($mediaId: Int, $status: MediaListStatus, $progress: Int) {
+        SaveMediaListEntry (mediaId: $mediaId, status: $status, progress: $progress) {
+            mediaId
+            status
+            progress
+        }
+    }
+    `,n={mediaId:parseInt(e.value.mediaId),status:e.value.status,progress:parseInt(e.value.progress)},t=await(await fetch("https://graphql.anilist.co",{method:"POST",headers:{"Content-Type":"application/json",Accept:"application/json",Authorization:`Bearer ${a}`},body:JSON.stringify({query:r,variables:n})})).json();console.error(t)}async function N(){const a=m.get("access_token"),r=`
+    mutation ($mediaId: Int, $status: MediaListStatus, $progress: Int) {
+        SaveMediaListEntry (mediaId: $mediaId, status: $status, progress: $progress) {
+            mediaId
+            status
+            progress
+        }
+    }
+    `,n={mediaId:parseInt(s.value.id),status:"PLANNING",progress:0};await(await fetch("https://graphql.anilist.co",{method:"POST",headers:{"Content-Type":"application/json",Accept:"application/json",Authorization:`Bearer ${a}`},body:JSON.stringify({query:r,variables:n})})).json(),I()}async function w(a){const r="",n=`https://api.malsync.moe/mal/manga/${a}`,t=await(await fetch(r+n)).json();console.log(t),h.value=t.Sites}function S(a){return O.sanitize(a)}const oe=()=>{e.value.progress++,e.value.progress>s.value.episodes&&(e.value.progress=s.value.episodes),e.value.progress===s.value.episodes&&(e.value.status="COMPLETED"),e.value.status==="PLANNING"&&(e.value.status="CURRENT")},ne=()=>{e.value.progress--,e.value.progress<0&&(e.value.progress=0),e.value.progress<s.value.episodes&&e.value.status!=="REPEATING"&&(e.value.status="CURRENT"),e.value.progress===0&&(e.value.status="PLANNING")};return(a,r)=>(d(),l(y,null,[o(s)?(d(),l("div",R,[i("div",D,[i("img",{src:o(s).bannerImage,alt:"",class:"manga-banner-image"},null,8,G)]),i("h1",x,g(o(s).title.english?o(s).title.english:o(s).title.romaji?o(s).title.romaji:o(s).title.native?o(s).title.native:""),1),i("div",U,[i("div",z,[i("img",{src:o(s).coverImage.large,alt:"Manga Cover Image",class:"manga-cover-image"},null,8,J)]),i("div",H,[i("p",{class:"manga-description",innerHTML:S(o(s).description)},null,8,F),i("h3",W,"Volumes: "+g(o(s).volumes),1)])])])):v("",!0),(o(e),o(s)?(d(),l("div",Y,[K,i("button",{class:"manga-completion-save",onClick:N,onTouchend:N,disabled:""}," Plan to watch ",32)])):v("",!0)),o(h)?(d(),l("div",Q,[X,(d(!0),l(y,null,b(o(h),(n,c)=>(d(),l("div",{key:c,class:"streaming-service"},[i("h2",Z,g(c),1),(d(!0),l(y,null,b(Object.values(n),(t,_)=>(d(),l("div",{key:_},[i("a",{href:t.url,target:"_blank"},g(t.title),9,ee)]))),128))]))),128))])):v("",!0)],64))}}),ce=q(se,[["__scopeId","data-v-ed1964bd"]]);export{ce as default};
---
+++ _nuxt/CujY3u4r.js
@@ -0,0 +1,62 @@
+import{n as O,r as g,u as R,q as x,h as D,c as l,s as a,a as s,t as u,v as y,x as M,y as B,z as U,F as f,A as k,o as d,p as G,e as z,B as h,_ as V}from"./VTd8GIPQ.js";import{p as J}from"./D4h8WKgh.js";const p=m=>(G("data-v-14bdf6ce"),m=m(),z(),m),H={key:0,class:"anime-details"},F={class:"anime-banner"},W=["src"],Y={class:"anime-title"},K={class:"anime-info"},Q={class:"anime-cover"},X=["src"],Z={class:"anime-text"},ee=["innerHTML"],se={class:"anime-episodes"},te={key:1,class:"anime-completion"},ae=p(()=>s("option",{value:"COMPLETED"},"Completed",-1)),oe=p(()=>s("option",{value:"PLANNING"},"Planning",-1)),ne=p(()=>s("option",{value:"CURRENT"},"Watching",-1)),ie=p(()=>s("option",{value:"REPEATING"},"Rewatching",-1)),re=[ae,oe,ne,ie],ce={class:"anime-progress"},le=p(()=>s("span",null," You have watched",-1)),de={class:"anime-progress-modifier"},pe=["max","disabled"],ue={key:2,class:"anime-completion"},me={key:3,class:"streaming-links"},ge=p(()=>s("h2",{class:"streaming-links-headline"},"Watch this anime:",-1)),he={class:"streaming-service-title"},ve=["href"],_e=O({__name:"[id]",setup(m){const t=g(),v=g(),I=g(),e=g(),A=R();x(()=>{const o=A.params.id.toString();w(parseInt(o)),S(parseInt(o))}),D(t,async(o,n)=>{o!==n&&(await b(),console.log(e.value))});function E(){switch(e.value.status){case"COMPLETED":e.value.progress=t.value.episodes;break;case"CURRENT":e.value.progress=0;break;case"PLANNING":e.value.progress=0;break;case"REPEATING":e.value.progress=0;break}}async function $(){const o=h.get("access_token"),n=`
+            query {
+                Viewer {
+                    id
+                    name
+                }
+            }
+        `,i={},c=await fetch("https://graphql.anilist.co",{method:"POST",headers:{"Content-Type":"application/json",Accept:"application/json",Authorization:`Bearer ${o}`},body:JSON.stringify({query:n,variables:i})});if(c.status===200){const r=await c.json();console.log(r),I.value=r.data.Viewer}}async function w(o){const r=await(await fetch("https://graphql.anilist.co",{method:"POST",headers:{"Content-Type":"application/json",Accept:"application/json"},body:JSON.stringify({query:`
+        query ($id: Int, $type: MediaType) {
+            Media (idMal: $id, type: $type) {
+                id
+                idMal
+                title {
+                    romaji
+                    english
+                    native
+                }
+                coverImage {
+                    extraLarge
+                    large
+                    medium
+                    color
+                }
+                description
+                episodes
+                bannerImage
+            }
+        }
+    `,variables:{id:o,type:"ANIME"}})})).json();console.log(r),t.value=r.data.Media}async function b(){await $();const o=h.get("access_token"),n=`
+    query ($userId: Int, $type: MediaType) {
+            MediaListCollection(userId: $userId, type: $type) {
+                lists {
+                    name
+                    entries {
+                        status
+                        score
+                        progress
+                        mediaId
+                        media {
+                            idMal
+                        }
+                    }
+                }
+            }
+        }
+    `,i={userId:I.value.id,type:"ANIME"},r=await(await fetch("https://graphql.anilist.co",{method:"POST",headers:{"Content-Type":"application/json",Accept:"application/json",Authorization:`Bearer ${o}`},body:JSON.stringify({query:n,variables:i})})).json();for(const _ in r.data.MediaListCollection.lists){const C=r.data.MediaListCollection.lists[_].entries.find(q=>q.media.idMal===t.value.idMal);if(C){e.value=C;return}}}async function N(){const o=h.get("access_token"),n=`
+    mutation ($mediaId: Int, $status: MediaListStatus, $progress: Int) {
+        SaveMediaListEntry (mediaId: $mediaId, status: $status, progress: $progress) {
+            mediaId
+            status
+            progress
+        }
+    }
+    `,i={mediaId:parseInt(e.value.mediaId),status:e.value.status,progress:parseInt(e.value.progress)},r=await(await fetch("https://graphql.anilist.co",{method:"POST",headers:{"Content-Type":"application/json",Accept:"application/json",Authorization:`Bearer ${o}`},body:JSON.stringify({query:n,variables:i})})).json();console.error(r)}async function T(){const o=h.get("access_token"),n=`
+    mutation ($mediaId: Int, $status: MediaListStatus, $progress: Int) {
+        SaveMediaListEntry (mediaId: $mediaId, status: $status, progress: $progress) {
+            mediaId
+            status
+            progress
+        }
+    }
+    `,i={mediaId:parseInt(t.value.id),status:"PLANNING",progress:0};await(await fetch("https://graphql.anilist.co",{method:"POST",headers:{"Content-Type":"application/json",Accept:"application/json",Authorization:`Bearer ${o}`},body:JSON.stringify({query:n,variables:i})})).json(),b()}async function S(o){const n="",i=`https://api.malsync.moe/mal/anime/${o}`,r=await(await fetch(n+i)).json();console.log(r),v.value=r.Sites}function L(o){return J.sanitize(o)}const P=()=>{e.value.progress++,e.value.progress>t.value.episodes&&(e.value.progress=t.value.episodes),e.value.progress===t.value.episodes&&(e.value.status="COMPLETED"),e.value.status==="PLANNING"&&(e.value.status="CURRENT")},j=()=>{e.value.progress--,e.value.progress<0&&(e.value.progress=0),e.value.progress<t.value.episodes&&e.value.status!=="REPEATING"&&(e.value.status="CURRENT"),e.value.progress===0&&(e.value.status="PLANNING")};return(o,n)=>(d(),l(f,null,[a(t)?(d(),l("div",H,[s("div",F,[s("img",{src:a(t).bannerImage,alt:"Anime Banner Image",class:"anime-banner-image"},null,8,W)]),s("h1",Y,u(a(t).title.english?a(t).title.english:a(t).title.romaji?a(t).title.romaji:a(t).title.native?a(t).title.native:""),1),s("div",K,[s("div",Q,[s("img",{src:a(t).coverImage.large,alt:"Anime Cover Image",class:"anime-cover-image"},null,8,X)]),s("div",Z,[s("p",{class:"anime-description",innerHTML:L(a(t).description)},null,8,ee),s("h3",se,"Episodes: "+u(a(t).episodes),1)])])])):y("",!0),a(e)?(d(),l("div",te,[M(s("select",{class:"anime-status","onUpdate:modelValue":n[0]||(n[0]=i=>a(e).status=i),onChange:E},re,544),[[B,a(e).status]]),s("h2",ce,[le,s("div",de,[M(s("input",{type:"number","onUpdate:modelValue":n[1]||(n[1]=i=>a(e).progress=i),min:"0",max:a(t).episodes,disabled:a(e).status=="COMPLETED"},null,8,pe),[[U,a(e).progress]]),s("div",{class:"anime-progress-btn-container"},[s("button",{class:"btn-increase",onClick:P}," ⏶ "),s("button",{class:"btn-decrease",onClick:j}," ⏷ ")])]),s("span",null,"out of "+u(a(t).episodes)+" Episodes.",1)]),s("button",{class:"anime-completion-save",onClick:N,onTouchend:N}," Save ",32)])):a(t)?(d(),l("div",ue,[s("button",{class:"anime-completion-save",onClick:T,onTouchend:T}," Plan to watch ",32)])):y("",!0),a(v)?(d(),l("div",me,[ge,(d(!0),l(f,null,k(a(v),(i,c)=>(d(),l("div",{key:c,class:"streaming-service"},[s("h2",he,u(c),1),(d(!0),l(f,null,k(Object.values(i),(r,_)=>(d(),l("div",{key:_},[s("a",{href:r.url,target:"_blank"},u(r.title),9,ve)]))),128))]))),128))])):y("",!0)],64))}}),Ie=V(_e,[["__scopeId","data-v-14bdf6ce"]]);export{Ie as default};
